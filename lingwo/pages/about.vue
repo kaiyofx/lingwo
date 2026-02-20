1b7f7fdd1ca7f0e1314bcef23932182f83212d3f
Dmitry <dima.trubnikov.82@bk.ru>
2026-02-20 21:34:13 +0800

    change domain

--- lingwo/pages/about.vue
+++ lingwo/pages/about.vue
@@ -38,11 +38,11 @@ useHead({
           </CardHeader>
           <CardContent >
             <a
-              href="mailto:contact@lingwo.ru"
+              href="mailto:contact@lingwoai.ru"
               class="flex items-center gap-2 text-primary hover:text-primary/80 font-medium transition-colors"
             >
               <Mail class="h-4 w-4 shrink-0" />
-              contact@lingwo.ru
+              contact@lingwoai.ru
             </a>
             <a
               href="https://t.me/kaiyofx"
